82057d72ca12a567a960d3565031a74b6d6fd67d
Vaishu <svaish2000@gmail.com>
2022-01-02 13:12:47 +0530

    Project Structure

---
+++ src/com/action/Invoker.scala
@@ -0,0 +1,13 @@
+package com.action
+
+import com.transformation.{ProductTypeImpl, RevenueImpl}
+
+object action {
+  def main(args:Array[String]): Unit ={
+    val productType = new ProductTypeImpl()
+    println("No of Product Type under Golf Equipment : " +productType.productTypeCount())
+
+    val revenue = new RevenueImpl()
+    println("Total revenue for the Retailer Country France is :"+revenue.totalRevenue())
+  }
+}
